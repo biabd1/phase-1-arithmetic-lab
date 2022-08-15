c2b0494643e2764eae8e2ec7e15ca56a43594904
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-15 08:13:53 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=2
+let num2=31
 
-// Write your code here
+const multiply = num1*num2
+
+const random = Math.floor(Math.random() * 10)+1;
+
+var num3=17;
+var num4=4;
+var mod = Math. floor(num3/num4); 
+var remainder = num3 % num4; 
+
+let value=(1,2,3,4,20)
+const max = Math.max(1, 2, 3, 4, 20);
